355bb9bc054297421ed37f67198ae8d7f7b619fc
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-11-30 13:56:58 +0700

    Add Random Meme Generator function

---
+++ functions/events/discord/message/meme.js
@@ -0,0 +1,15 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+const gis = require('g-i-s');
+
+if (context.params.event.content.startsWith('/meme')) {
+let memeSearch = context.params.event.content.split(' ').slice(1).join(' ') + 'meme';
+
+await new Promise((resolve, reject) => {
+    gis(`${memeSearch}`, async function findImage(err, res) {
+      await lib.discord.channels['@0.1.0'].messages.create({
+        channel_id: context.params.event.channel_id,
+        content: `${res[Math.floor(Math.random()*res.length)].url}`
+      });
+    })
+  });
+}
